70de5d46ea5451ce337a47e42b507454bdd67c3d
ogeti siva satyanarayana <109501411+sivaogeti@users.noreply.github.com>
2025-07-19 20:35:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from PIL import UnidentifiedImageError
 
 from backend.media_gen import generate_image, generate_audio, generate_video
 from backend.media_gen import sanitize_filename, translate_text
+import re
 
 load_dotenv()
 
