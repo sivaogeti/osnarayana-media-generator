17f90bfd71972d2d7d8389e55cf6d92c265cd25d
ogeti siva satyanarayana <109501411+sivaogeti@users.noreply.github.com>
2025-07-20 00:52:06 +0530

    Update media_gen.py

--- backend/media_gen.py
+++ backend/media_gen.py
@@ -8,7 +8,7 @@ from PIL import Image, UnidentifiedImageError
 from io import BytesIO
 from dotenv import load_dotenv
 from moviepy.editor import ImageClip, AudioFileClip
-from elevenlabs import set_api_key, Voice, VoiceSettings, generate_audio  # Updated import
+from elevenlabs import Voice, VoiceSettings, generate_audio  # Updated imports
 from googletrans import Translator
 
 # Load env vars
